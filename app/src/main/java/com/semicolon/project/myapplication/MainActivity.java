33e8992cff3537e37a7d4ed12f7d14b175c0927e
jun34723 <43941511+jun34723@users.noreply.github.com>
2018-11-01 10:24:41 +0900

    바코드 리더 구현

--- app/src/main/java/com/semicolon/project/myapplication/MainActivity.java
+++ app/src/main/java/com/semicolon/project/myapplication/MainActivity.java
@@ -1,13 +1,46 @@
 package com.semicolon.project.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //시작
+
+        findViewById(R.id.btn_start_qrcode_reader).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQRCode();
+            }
+        });
+
+    }
+
+    public void startQRCode() {
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE) {
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if (result == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, null);
+        }
     }
 }
+
