29fbb7bf9ff91cffb1c422d79d6fcd60aeb9c04a
KUMJU <43941369+KUMJU@users.noreply.github.com>
2018-11-01 18:49:49 +0900

    합본

--- app/src/main/java/com/semicolon/project/myapplication/MainActivity.java
+++ app/src/main/java/com/semicolon/project/myapplication/MainActivity.java
@@ -1,28 +1,48 @@
 package com.semicolon.project.myapplication;
 
 import android.content.Intent;
-import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Animation fab_open, fab_close;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1, fab2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //시작
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
 
-        findViewById(R.id.btn_start_qrcode_reader).setOnClickListener(new View.OnClickListener() {
+        fab =  findViewById(R.id.fab);
+        fab1 =  findViewById(R.id.fab1);
+        fab2 =  findViewById(R.id.fab2);
+
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+
+
+        /*findViewById(R.id.btn_start_qrcode_reader).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startQRCode();
             }
         });
+        */
 
     }
 
@@ -42,4 +62,43 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, null);
         }
     }
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fab:
+                anim();
+                Toast.makeText(this, "Floating Action Button", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fab1:
+                anim();
+                Toast.makeText(this, "Button1", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this,InputActivity.class));
+                break;
+            case R.id.fab2:
+                anim();
+                Toast.makeText(this, "Button2", Toast.LENGTH_SHORT).show();
+                startQRCode();
+                break;
+        }
+
+
+    }
+
+    public void anim() {
+
+        if (isFabOpen) {
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+        }
+    }
 }
+
