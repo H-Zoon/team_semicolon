a672d0fbdc35fe85f4f3eb288865a9a83b47b6fe
jun <jun34724@gmail.com>
2018-11-19 01:07:21 +0900

    사이드바 구현

---
+++ app/src/main/java/com/semicolon/project/myapplication/DrawerItemClickListener.java
@@ -0,0 +1,36 @@
+package com.semicolon.project.myapplication;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+    private ListView lvNavList;
+    private FrameLayout flContainer;
+
+    public void onItemClick(AdapterView<?> adapter, View view, int position,
+                            long id) {
+        switch (position) {
+            case 0:
+                flContainer.setBackgroundColor(Color.parseColor("#A52A2A"));
+                break;
+            case 1:
+                flContainer.setBackgroundColor(Color.parseColor("#5F9EA0"));
+                break;
+            case 2:
+                flContainer.setBackgroundColor(Color.parseColor("#556B2F"));
+                break;
+            /*case 3:
+                flContainer.setBackgroundColor(Color.parseColor("#FF8C00"));
+                break;
+            case 4:
+                flContainer.setBackgroundColor(Color.parseColor("#DAA520"));
+                break;
+*/
+        }
+
+    }
+}
